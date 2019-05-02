146ff5d4a3d18b9e6db5bed1884a10eb1a74faeb
Nathan Malnoury <nmalnoury@skapane.com>
2019-05-02 17:49:26 +0200

    fix(cli) : typos and missing logic.

--- webmupdf/cli.py
+++ webmupdf/cli.py
@@ -18,12 +18,12 @@ if __name__ == '__main__':
     file_binary = sys.stdin.read()
 
     if args.page_count:
-        sys.stdout.write(pickle.dumps(converter.page_count(file_binary, filetype=args.type), pickle.HIGHEST_PROTOCOL))
+        sys.stdout.write(str(converter.page_count(file_binary, filetype=args.type)))
 
-    if args.page != -1:
+    elif args.page != -1:
         # user sent a page to be converted
         np_img = converter.get_page(file_bin=file_binary, file_type=args.type, page_num=args.page, width_output_file=args.width)
-        sys.stdout.write(pickle.dump(np_img, pickle.HIGHEST_PROTOCOL))
-
-    np_array = converter.get_pages(file_bin=file_binary, file_type=args.type, width_output_file=args.width)
-    sys.stdout.write(pickle.dumps(np_array, pickle.HIGHEST_PROTOCOL))
+        sys.stdout.write(pickle.dumps(np_img, pickle.HIGHEST_PROTOCOL))
+    else:
+        np_array = converter.get_pages(file_bin=file_binary, file_type=args.type, width_output_file=args.width)
+        sys.stdout.write(pickle.dumps(np_array, pickle.HIGHEST_PROTOCOL))
