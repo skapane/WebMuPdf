99e911c54b3d598c934617d8d8db669d46a7d489
Renaud Loiseleux <rloiseleux@skapane.com>
2020-11-20 18:31:59 +0100

    chore(version): 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="webmupdf",
-    version="2.0.0",
+    version="2.0.1",
     author="Nathan Malnoury, Yassine Fikrat, Renaud Loiseleux",
     description="A web service for PyMuPDF",
     long_description_content_type="text/markdown",
