b6289674fd13dbbeac95b900b03cd558b12f8a16
Mary Deroost <mderoost@skapane.ai>
2021-06-30 11:33:27 +0200

    chore(version): 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="webmupdf",
-    version="2.1.0-SNAPSHOT",
+    version="2.1.0",
     author="Nathan Malnoury, Yassine Fikrat, Renaud Loiseleux",
     description="A web service for PyMuPDF",
     long_description_content_type="text/markdown",
