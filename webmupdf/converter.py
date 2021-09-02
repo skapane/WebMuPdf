5c53a5a9705557bf025d1bf66344c7b77552fdaf
Mary Deroost <mderoost@skapane.ai>
2021-09-02 10:58:43 +0200

    fix: remove unused variable

--- webmupdf/converter.py
+++ webmupdf/converter.py
@@ -76,8 +76,6 @@ def get_page(file_bin, page_num, file_type, width_output_file, password):
     page_width = page.MediaBoxSize.x
     page_area = page_height * page_width
 
-    smallest_side = min(page_height, page_width)
-
     # Get blocks with image bboxes only (no actual image is loaded)
     blocks = page.getText('BLOCKS', flags=7)
 
