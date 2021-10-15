17cbf586802b512ad3f836770b1e2f942dfbd8c8
Mary Deroost <mderoost@skapane.ai>
2021-10-15 14:38:10 +0200

    revert changes

--- webmupdf/cli.py
+++ webmupdf/cli.py
@@ -2,8 +2,6 @@
 # encoding : utf-8
 import argparse
 
-from webmupdf.split import split_pages
-
 try:
     import cPickle as pickle
 except ImportError:
@@ -26,12 +24,9 @@ if __name__ == '__main__':
     parser.add_argument("--width", help="Width in pixel of the output document", type=int, default=2048)
     parser.add_argument("--page", help='if only one page to process, use this argument. Pages are indexed from 0',
                         type=int, default=-1)
-    parser.add_argument("--start_page", help='Number of the first page to start splitting at', type=int, default=0)
-    parser.add_argument("--end_page", help='Number of the last page (included) to stop splitting at', type=int,
-                        default=0)
     parser.add_argument("--page-count", help='count number of pages in a document', action='store_true', default=False)
     parser.add_argument("--output", help='Type of output to give', type=str, default="np_array",
-                        choices=["np_array", "ConvertedPage", "pdf_document"])
+                        choices=["np_array", "ConvertedPage"])
     args = parser.parse_args()
     file_binary = sys_stdin.read()
 
@@ -40,32 +35,21 @@ if __name__ == '__main__':
 
     elif args.page != -1:
 
-        if args.output == "pdf_document":
-            pdf_file = split_pages(file_bin=file_binary,
-                                   start_page_num=args.start_page,
-                                   end_page_num=args.end_page,
-                                   file_type=args.type)
+        # user sent a page to be converted
+        converted_page = converter.get_page(
+            file_bin=file_binary,
+            file_type=args.type,
+            page_num=args.page,
+            width_output_file=args.width
+        )
+
+        if args.output == "ConvertedPage":
             sys_stdout.write(pickle.dumps(
-                    pdf_file,
-                    pickle.HIGHEST_PROTOCOL
-                ))
-
-        else:
-            # user sent a page to be converted
-            converted_page = converter.get_page(
-                file_bin=file_binary,
-                file_type=args.type,
-                page_num=args.page,
-                width_output_file=args.width
-            )
-
-            if args.output == "ConvertedPage":
-                sys_stdout.write(pickle.dumps(
-                    converted_page,
-                    pickle.HIGHEST_PROTOCOL
-                ))
-            elif args.output == "np_array":
-                sys_stdout.write(pickle.dumps(
-                    converted_page.np_render,
-                    pickle.HIGHEST_PROTOCOL
-                ))
+                converted_page,
+                pickle.HIGHEST_PROTOCOL
+            ))
+        elif args.output == "np_array":
+            sys_stdout.write(pickle.dumps(
+                converted_page.np_render,
+                pickle.HIGHEST_PROTOCOL
+            ))
